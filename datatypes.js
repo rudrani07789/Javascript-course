a81b76ceefeeb607d852a3696859fc16201c67a1
rudrani07789 <145859836+rudrani07789@users.noreply.github.com>
2026-06-20 07:31:06 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(2+3) //we are using nodejs, not browser
+
+
+let name="Rini";
+let age=26
+let isLogin=false
+
+//number
+//string ="123"
+//bigint
+//boolen-True/false
+//null
+//undefined
+//symbol= unqiue
+
+//object
+
+console.log(typeof "Rini");
+console.log(typeof null); //object type
+console.log(typeof undefined); //unfeined
---
+++ variables-constants.js
@@ -0,0 +1,13 @@
+const accountId= 144356;
+let accountEmail="rini1556@gmail.com";
+var accountPassword="1234"; // prefer not to use var
+accountCity="Bangalore";
+let accountState;
+//accountId=2; NOT allowed
+
+accountEmail="1234@gmail.com";
+accountPassword="23t5";
+accountCity="Kolkata";
+
+//console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
