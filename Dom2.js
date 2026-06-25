f5f00b5f8e8237801f00617cc79052cd30f46061
rudrani07789 <145859836+rudrani07789@users.noreply.github.com>
2026-06-25 09:51:30 +0530

    Add files via upload

---
+++ Dom2.js
@@ -0,0 +1,18 @@
+const parentdiv=document.querySelector(".parent")
+/*console.log(parentdiv)
+console.log(parentdiv.children)
+//console.log(parentdiv.children[0].innerHTML)
+
+for (let index = 0; index < parentdiv.children.length; index++) {
+    console.log(parentdiv.children[index].innerHTML);
+    
+}*/
+parentdiv.children[1].style.color="orange"
+//console.log(parentdiv.firstElementChild)
+//console.log(parentdiv.lastElementChild)
+const dayOne=document.querySelector(".day")
+/*console.log(dayOne)
+console.log(dayOne.parentElement)
+console.log(dayOne.nextElementSibling)*/
+
+console.log(parentdiv.childNodes)
---
+++ Doms.js
@@ -0,0 +1,18 @@
+//document.getElementsByClassName
+//document.querySelector('h2')
+//document.querySelector('#title')
+//node list : temList
+/*const temList=document.querySelectorAll('li')
+//temList[0].style.backgroundColor='green'
+temList.forEach(function(l){
+     l.style.backgroundColor='pink'
+})*/
+
+/*const tempClass=document.getElementsByClassName('list')
+const newArr=Array.from(tempClass)
+newArr.forEach(function(l){
+    l.style.backgroundColor='pink'
+})*/
+
+
+
